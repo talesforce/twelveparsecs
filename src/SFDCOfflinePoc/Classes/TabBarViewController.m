6ec4e67467500d080f541eabd915da8748ddd553
n1k1tung <n1k1tung4tc@gmail.com>
2016-02-22 17:42:03 +0300

    small updates

--- src/SFDCOfflinePoc/Classes/TabBarViewController.m
+++ src/SFDCOfflinePoc/Classes/TabBarViewController.m
@@ -63,7 +63,7 @@ static NSString* pin = @"012016";
     [reach startNotifier];
 
     if (![reach isReachable]) {
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             noConnection = YES;
             for (UINavigationController* navc in self.viewControllers) {
                 BaseListViewController* vc = navc.viewControllers[0];
@@ -78,7 +78,7 @@ static NSString* pin = @"012016";
 
 - (void)showAlert {
     if (noConnection) {
-        alertView = [UIAlertController alertControllerWithTitle:@"No connection" message:@"Enter ping"
+        alertView = [UIAlertController alertControllerWithTitle:@"No connection" message:@"Enter pin"
                                                  preferredStyle:UIAlertControllerStyleAlert];
         [alertView addTextFieldWithConfigurationHandler:^(UITextField *textField) {
             textField.placeholder = @"Enter pin";
